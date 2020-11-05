fadc73ba7cdb97ac0f8872bf4c17d5b037b800ae
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-11-05 10:33:24 -0500

    Create downloadArray.R

---
+++ downloadArray.R
@@ -0,0 +1,23 @@
+require(downloader)
+library(curl)
+
+options(stringsAsFactors = FALSE)
+my.path="/pfs/out"
+
+dir1 <- "ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/dixa/DrugMatrix/archive/hepatocyte/"
+
+dir.create(file.path(my.path, "raw"), showWarnings=FALSE, recursive=TRUE)
+
+h = new_handle(dirlistonly=TRUE)
+con = curl(dir1, "r", h)
+tbl = read.table(con, stringsAsFactors=TRUE, fill=TRUE)
+close(con)
+head(tbl)
+
+urls <- paste0(dir1, tbl$V1)
+fls = basename(urls)
+fls <- fls[1:5]
+
+lapply(fls, function(filename){
+  curl_download(paste(dir1, filename, sep = ""), destfile = paste0(file.path(my.path, "raw"),"/",filename), handle = h)
+})
